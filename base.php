99052257a0414747adfda5f9e48565acf194ea03
Scott Hilbert <github@shilbert.com>
2012-09-22 13:56:25 -0400

    Updated google analytics code

--- base.php
+++ base.php
@@ -44,17 +44,20 @@ function site_footer() {
 		
 			</div> <!-- end content -->
 		</div> <!-- end contentlayer -->
-		<script type="text/javascript">
-		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-		</script>
-		<script type="text/javascript">
-		try {
-		var pageTracker = _gat._getTracker("UA-15233927-1");
-		pageTracker._setDomainName(".shilbert.com");
-		pageTracker._trackPageview();
-		} catch(err) {}
-		</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-15233927-1']);
+  _gaq.push(['_setDomainName', 'shilbert.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 	</body>
 </html>
 	<?php
